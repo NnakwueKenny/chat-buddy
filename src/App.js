eef6b51892692b1b73e7a31e9c7b0e7995d9d05a
Kenny Nnakwue <nnakwuekenny@gmail.com>
2022-12-27 00:22:49 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import logo  from './assets/images/logo.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header uppercase">
+        Chat Buddy
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
