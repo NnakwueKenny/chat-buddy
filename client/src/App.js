5981e5d91dd1b565a321e7aa05d7e3fda47f7a40
Kenny Nnakwue <nnakwuekenny@gmail.com>
2023-02-14 19:55:48 +0100

    App.js && TopNav.js Updated

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 import { Box, Container } from '@mui/system';
-import { purple } from '@mui/material/colors';
+import { purple, red, grey } from '@mui/material/colors';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import TopNav from './components/Navigations/TopNav';
-import { useMediaQuery } from '@mui/material';
+import { Toolbar, Typography, useMediaQuery } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
@@ -16,7 +16,18 @@ function App() {
       createTheme({
         palette: {
           primary: {
-            main: purple[500]
+            // main: purple[500],
+            // main: grey[500],
+            main: purple[500],
+            light: '#757ce8',
+            dark: '#002884',
+            // dark: purple[200],
+            contrastText: '#fff',
+          },
+          secondary: {
+            main: '#f44336',
+            light: '',
+            dark: '#000000'
           },
           mode: prefersDarkMode ? 'dark' : 'light',
         },
@@ -28,9 +39,12 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline/>
       <div className="">
-        <Container disableGutters={true} color='primary'>
-          <Box sx={{ padding: 0, height: '100vh', width: '100%', margin: 0, color: 'primary' }}>
+        <Container disableGutters={true} >
+          <Box sx={{ height: '100vh', width: '100%' }}>
             <TopNav/>
+            <Toolbar />
+            <Toolbar />
+            <Box>All other should be here of course components here</Box>
           </Box>
         </Container>
       </div>
--- client/src/components/Navigations/TopNav.jsx
+++ client/src/components/Navigations/TopNav.jsx
@@ -1,30 +1,85 @@
-import { Stack } from '@mui/system'
+import { Box, Stack } from '@mui/system';
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import { AppBar, IconButton, Tab, Tabs, Toolbar, Typography } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
 const TopNav = () => {
-  return (
-    <AppBar>
-      <Toolbar>
-        <Stack direction='row'>
-          <Typography>Chat Buddy</Typography>
-          <IconButton aria-label="delete" variant='outlined' color='primary'>
-            <DeleteIcon />
-          </IconButton>
-        </Stack>
-    </Toolbar>
-    <Toolbar>
-        <Stack direction='row'>
-          <Typography>Chat Buddy</Typography>
-          <IconButton aria-label="delete">
-            <DeleteIcon />
-          </IconButton>
-        </Stack>
-      </Toolbar>
-    </AppBar>
-  )
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    return (
+        <AppBar sx={{ backgroundColor: 'transparent' }}>
+            <Toolbar>
+                <Box direction='column'>
+                    <Stack direction='row'>
+                        <Box>
+                            <Typography color='primary'>Chat Buddy</Typography>
+                        </Box>
+                        <Box>
+                            <IconButton aria-label="delete" variant='outlined'>
+                                <DeleteIcon />
+                            </IconButton>
+                        </Box>
+                    </Stack>
+                    <Box sx={{ width: '100%', color: 'green' }}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                                <Tab label="Item One" {...a11yProps(0)} />
+                                <Tab label="Item Two" {...a11yProps(1)} />
+                                <Tab label="Item Three" {...a11yProps(2)} />
+                            </Tabs>
+                        </Box>
+                        <TabPanel value={value} index={0}>
+                            Item One
+                        </TabPanel>
+                        <TabPanel value={value} index={1}>
+                            Item Two
+                        </TabPanel>
+                        <TabPanel value={value} index={2}>
+                            Item Three
+                        </TabPanel>
+                    </Box>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    )
 }
 
 export default TopNav;
